dd42db660ab58c24494a63b2927c6f391c2213b2
Yatharth Sathya <yascole2010@gmail.com>
2025-08-30 12:57:28 -0700

    Created promptLog.cs

---
+++ config/promptLog.cs
@@ -0,0 +1,9 @@
+// Logging the prompt in C# just for fun
+using System;
+
+class PromptLog {
+    static void Main() {
+        string prompt = "Cozy rustic kitchen";
+        Console.WriteLine($"Generating interior design for: {prompt}");
+    }
+}
